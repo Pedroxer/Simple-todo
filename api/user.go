a95f0468c6d6a3246bf0d721501d8d779d21350c
Pedroxer <danila.melnikov.53@gmail.com>
2023-08-12 02:28:59 +0300

    Created login handler

--- api/user.go
+++ api/user.go
@@ -30,7 +30,7 @@ func newUserResp(user sqlc.User) UserResponse {
 		CreatedAt: user.CreatedAt,
 	}
 }
-func (server *Server) CreateUser(ctx *gin.Context) {
+func (server *Server) createUser(ctx *gin.Context) {
 	var req CreateUserRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 
@@ -76,6 +76,49 @@ func (server *Server) getUser(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, user)
 }
 
-//func (server *Server) LoginUser(ctx *gin.Context){
-//	var
-//}
+type loginUserReq struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type loginUserResp struct {
+	AccessToken string       `json:"access_token"`
+	User        UserResponse `json:"user"`
+}
+
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req loginUserReq
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	} // 1. получаем пользователя
+	user, err := server.db.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	// 2. Проверяем пароль
+	err = util.CheckPassword(req.Password, user.Password)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
+	// 3. Создаём токен
+	accessToken, err := server.tokenMaker.CreateToken(user.Username, server.config.TokenDuration)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	resp := loginUserResp{
+		AccessToken: accessToken,
+		User:        newUserResp(user),
+	}
+	ctx.JSON(http.StatusOK, resp)
+
+}
